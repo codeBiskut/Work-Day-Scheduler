f61040a78f4756ac1dcbc9b7210a38883c17aba0
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-08-27 17:44:52 -0400

    added timeblocks from 9-5

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@
 // if  moment() < time-blokcs => future
 //step 4: grap the textarea value and store it in localstorage
 
-var timeBlocksArr=[9,10,11] //translate time-blocks to military times
+var timeBlocksArr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24] //translate time-blocks to military times
 
 var currenthour= moment().hour()
            
