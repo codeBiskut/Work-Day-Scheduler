4aedd675fc56a5968bf3d5c80ab427e08ff554d3
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-08-27 17:55:33 -0400

    cleaned uup script

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,14 @@
-//step 1: put current date on web page using moment()
-//step 2: compare the current hour with time-blocks
-//ste  3: translate time-blocks to military times
-//step 4: use the css classes for present, past or future
-//compare the time-blocks with moment()
-// if moment() > time-blocks =>past
-// if moment() == time-block => Present
-// if  moment() < time-blokcs => future
-//step 4: grap the textarea value and store it in localstorage
+
 
 var timeBlocksArr=[9,10,11,12,13,14,15,16,17] //translate time-blocks to military times
 
 var currenthour= moment().hour()
            
-var currentDay =$("#currentDay")  // document.querySelector("#currentDay")
+var currentDay =$("#currentDay")  
 console.log(currenthour)
 
 var systemTime=moment().format("dddd, MMMM Do")
-currentDay.text(systemTime)   // currentDay.textContent=systemTime
+currentDay.text(systemTime)   
 
 function displayTimeBlockColors(){
     for(var i=0; i < timeBlocksArr.length;i++){
